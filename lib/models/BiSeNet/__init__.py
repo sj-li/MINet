97857af4d6f595c0e520d7c067063fc9703e0fb3
sj-li <lishijie1234@outlook.com>
2020-07-11 00:02:15 +0200

    add image-based methods

---
+++ lib/models/BiSeNet/__init__.py
@@ -0,0 +1,1 @@
+from .build_BiSeNet import BiSeNet
--- lib/models/__init__.py
+++ lib/models/__init__.py
@@ -1,4 +1,10 @@
 from .MINet import MINet
 
+# image based method
+from .DenseASPP import DenseASPP
+from .deeplab import DeepLab
+from .pspnet import PSPNet
+from .BiSeNet import BiSeNet
+
 def get_model(model):
     return eval(model)
