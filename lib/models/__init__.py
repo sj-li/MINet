be0b9b21102778b350db165a62ddffd77e189630
Shijie Li <lishijie1234@outlook.com>
2021-12-10 16:56:49 +0100

    Update __init__.py

--- lib/models/__init__.py
+++ lib/models/__init__.py
@@ -1,10 +1,4 @@
 from .MINet import MINet
 
-# image based method
-from .DenseASPP import DenseASPP
-from .deeplab import DeepLab
-from .pspnet import PSPNet
-from .BiSeNet import BiSeNet
-
 def get_model(model):
     return eval(model)
